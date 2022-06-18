a9f4b422635e44d279e76a53ba6888839767820a
Rounak Singh <rounaksingh9431738764@gmail.com>
2022-06-18 08:26:23 +0530

    Got all the value required, thanks to expo

--- Service/GetLocationInfo.js
+++ Service/GetLocationInfo.js
@@ -6,6 +6,34 @@ export default function GetLocationInfo() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
 
+  const LocationValue = {
+    coords: {
+      accuracy: 20.899999618530273,
+      altitude: 580,
+      altitudeAccuracy: 26.625600814819336,
+      heading: 0,
+      latitude: 23.2947586,
+      longitude: 85.3237117,
+      speed: 0,
+    },
+    mocked: false,
+    timestamp: 1655520772594,
+  };
+
+  const fineLocation = {
+    city: "Ranchi",
+    country: "India",
+    district: "Hesag",
+    isoCountryCode: "IN",
+    name: "78VF+WG9",
+    postalCode: "834003",
+    region: "Jharkhand",
+    street: null,
+    streetNumber: null,
+    subregion: "Ranchi",
+    timezone: null,
+  };
+
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
@@ -15,6 +43,11 @@ export default function GetLocationInfo() {
       }
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+      let fineLocation = await Location.reverseGeocodeAsync({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+      console.log(location);
     })();
   }, []);
 
